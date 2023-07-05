378eb6e13155500085d8ec5ee76410ee55b9ea65
bhanuprakash4600 <ancg5q2ym6rdm2fxxwnffolagvrjq4w5tfn3tsozf2rm6t35ayvq>
2023-07-05 14:58:45 +0530

    bhanu

---
+++ .ipynb_checkpoints/storage_account-checkpoint.tf
@@ -0,0 +1,23 @@
+# Define the provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "Jupyter_Testing" "example" {
+  name     = "example-resource-group"
+  location = "West Europe"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = "jupytertesting460"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = "Dev"
+  }
+}
